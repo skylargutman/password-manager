a0ce477c85119c2805cef5a9c3d52da5d17e6c86
Scottie Gutman <Scottie@signaturecomputer.com>
2025-06-09 14:10:40 -0400

    Added error handling

--- main.py
+++ main.py
@@ -2,6 +2,29 @@ from tkinter import *
 from tkinter import messagebox
 from password_generator import PasswordGenerator
 import pyperclip
+import json
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    if website_entry.get() == "":
+        messagebox.showinfo("Empty", "Please enter a website name")
+    else:
+        try:
+            with open("password.json") as password_file:
+                pw_s = json.load(password_file)
+        except FileNotFoundError:
+            messagebox.showinfo("No Passwords", "No password file found")
+            website_entry.focus()
+        else:
+            try:
+                email = pw_s[website_entry.get().lower()]["email"]
+                password = pw_s[website_entry.get().lower()]["password"]
+            except KeyError:
+                messagebox.showinfo("No record", "There is no record of this website")
+            else:
+                messagebox.showinfo(f"Your {website_entry.get()} Credentials:", f"Email: {email}\n"
+                                                     f"Password: {password}")
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -16,7 +39,13 @@ def save_password():
     website_str = website_entry.get()
     email_str = email_entry.get()
     password_str = password_entry.get()
-    password_details = f"{website_str} | {email_str} | {password_str}\n"
+    # password_details = f"{website_str} | {email_str} | {password_str}\n"
+    new_password = {
+        website_str.lower():{
+            "email": email_str,
+            "password": password_str
+        }
+    }
 
     if len(website_str)==0 or len(email_str)==0 or len(password_str)==0:
         messagebox.showinfo(title="Empty", message="Please fill in all the entries")
@@ -27,10 +56,22 @@ def save_password():
                                                         f"Email: {email_str}\n"
                                                         f"Password: {password_str}")
         if ok_to_save:
-            with open("password.txt", "a") as password_file:
-                password_file.write(password_details)
-                website_entry.delete(0,END)
-                password_entry.delete(0,END)
+            try:
+                with open("password.json", "r") as password_file:
+                    pw_s = json.load(password_file)
+
+            except FileNotFoundError:
+                with open("password.json","w") as password_file:
+                    # noinspection PyTypeChecker
+                    json.dump(new_password, password_file, indent=4)
+            else:
+                pw_s.update(new_password)
+                with open("password.json", "w") as password_file:
+                    # noinspection PyTypeChecker
+                    json.dump(pw_s, password_file, indent=4)
+            finally:
+                    website_entry.delete(0,END)
+                    password_entry.delete(0,END)
         else:
             website_entry.focus()
 
@@ -48,27 +89,30 @@ canvas.grid(column=0, row=0, columnspan=3)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-website_entry = Entry(width=48)
-website_entry.grid(column=1,row=1,columnspan=2)
+website_entry = Entry(width=30)
+website_entry.grid(column=1,row=1,)
 website_entry.focus()
+
+search_button = Button(text="Search", width=15, command=find_password)
+search_button.grid(column=2, row=1)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
 
-email_entry = Entry(width=48)
+email_entry = Entry(width=50)
 email_entry.grid(column=1,row=2,columnspan=2)
 email_entry.insert(END, "skylar@signaturecomputer.com")
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
-password_entry = Entry(width=21)
+password_entry = Entry(width=30)
 password_entry.grid(column=1,row=3)
 
-generate_password_button = Button(text="Generate Password", width=21, command=generate_password)
+generate_password_button = Button(text="Generate Password", width=15, command=generate_password)
 generate_password_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=40, command=save_password)
+add_button = Button(text="Add", width=42, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2)
 
 
